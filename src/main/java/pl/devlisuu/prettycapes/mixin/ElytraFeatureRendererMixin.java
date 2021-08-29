8e1e2db75bfaa9b3e8dfd5024a46a125d32d5eb7
DevLisuu <everyaccountholder@wp.pl>
2021-08-29 11:19:07 +0200

    Fixed invisibilty

--- src/main/java/pl/devlisuu/prettycapes/mixin/ElytraFeatureRendererMixin.java
+++ src/main/java/pl/devlisuu/prettycapes/mixin/ElytraFeatureRendererMixin.java
@@ -11,6 +11,8 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
@@ -26,8 +28,15 @@ public abstract class ElytraFeatureRendererMixin<T extends LivingEntity, M exten
     private void forceElytraRendering(Args args, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, T livingEntity, float f, float g, float h, float j, float k, float l) {
         if(livingEntity instanceof PlayerEntity) {
             AbstractClientPlayerEntity abstractPlayer = (AbstractClientPlayerEntity)livingEntity;
+
             if(abstractPlayer.canRenderCapeTexture() && abstractPlayer.getCapeTexture() != null && abstractPlayer.isPartVisible(PlayerModelPart.CAPE)) {
-                args.set(0, livingEntity.getEquippedStack(EquipmentSlot.CHEST).getItem());
+                ItemStack itemStack = livingEntity.getEquippedStack(EquipmentSlot.CHEST);
+
+                if(!(abstractPlayer.isInvisible())) {
+                    args.set(0, livingEntity.getEquippedStack(EquipmentSlot.CHEST).getItem());
+                }else if(!(itemStack.isOf(Items.AIR))) {
+                    args.set(0, livingEntity.getEquippedStack(EquipmentSlot.CHEST).getItem());
+                }
             }
         }
     }
