e82987958090ea79120a820da32bbcafe9533421
DevLisuu <everyaccountholder@wp.pl>
2021-08-29 18:59:51 +0200

    Code cleanup

--- src/main/java/pl/devlisuu/prettycapes/mixin/ElytraFeatureRendererMixin.java
+++ src/main/java/pl/devlisuu/prettycapes/mixin/ElytraFeatureRendererMixin.java
@@ -4,9 +4,6 @@ import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.PlayerModelPart;
 import net.minecraft.client.render.entity.feature.ElytraFeatureRenderer;
-import net.minecraft.client.render.entity.feature.FeatureRenderer;
-import net.minecraft.client.render.entity.feature.FeatureRendererContext;
-import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
@@ -19,10 +16,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(ElytraFeatureRenderer.class)
-public abstract class ElytraFeatureRendererMixin<T extends LivingEntity, M extends EntityModel<T>> extends FeatureRenderer<T, M> {
-    public ElytraFeatureRendererMixin(FeatureRendererContext<T, M> context) {
-        super(context);
-    }
+public abstract class ElytraFeatureRendererMixin<T extends LivingEntity> {
 
     @ModifyArgs(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z"))
     private void forceElytraRendering(Args args, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, T livingEntity, float f, float g, float h, float j, float k, float l) {
