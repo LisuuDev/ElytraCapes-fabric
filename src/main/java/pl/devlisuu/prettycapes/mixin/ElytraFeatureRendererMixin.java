045bbaa5867a9fb720fce290b30094f7652114e5
Lisu <ruzia12349@wp.pl>
2022-01-12 17:10:01 +0100

    code readjustments

--- src/main/java/pl/devlisuu/prettycapes/mixin/ElytraFeatureRendererMixin.java
+++ src/main/java/pl/devlisuu/prettycapes/mixin/ElytraFeatureRendererMixin.java
@@ -8,7 +8,6 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,17 +17,19 @@ import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 @Mixin(ElytraFeatureRenderer.class)
 public abstract class ElytraFeatureRendererMixin<T extends LivingEntity> {
 
-    @ModifyArgs(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z"))
+    @ModifyArgs(method = "render*", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z"))
     private void forceElytraRendering(Args args, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, T livingEntity, float f, float g, float h, float j, float k, float l) {
         if(livingEntity instanceof PlayerEntity) {
+
+            //Convert entity to player, so I can get more booleans in my if statements
             AbstractClientPlayerEntity abstractPlayer = (AbstractClientPlayerEntity)livingEntity;
 
-            if(abstractPlayer.canRenderCapeTexture() && abstractPlayer.getCapeTexture() != null && abstractPlayer.isPartVisible(PlayerModelPart.CAPE)) {
-                ItemStack itemStack = livingEntity.getEquippedStack(EquipmentSlot.CHEST);
+            if(abstractPlayer.canRenderCapeTexture() && abstractPlayer.canRenderElytraTexture() && abstractPlayer.getCapeTexture() != null && abstractPlayer.isPartVisible(PlayerModelPart.CAPE)) {
 
-                if(!(abstractPlayer.isInvisible())) {
+                if(!livingEntity.isInvisible()) {
                     args.set(0, livingEntity.getEquippedStack(EquipmentSlot.CHEST).getItem());
-                }else if(!(itemStack.isOf(Items.AIR))) {
+                }else if(!livingEntity.getEquippedStack(EquipmentSlot.CHEST).isOf(Items.AIR)) {
+                    //Show cape if invisible but chest slot is not empty
                     args.set(0, livingEntity.getEquippedStack(EquipmentSlot.CHEST).getItem());
                 }
             }
