dd8c964059b2ec5e4c9c0fff7d5c6ee8d508f6aa
Lisu <everyaccountholder@wp.pl>
2022-07-04 12:48:41 +0200

    1.19 update

--- src/main/java/pl/devlisuu/elytracapes/config/ConfigScreen.java
+++ src/main/java/pl/devlisuu/elytracapes/config/ConfigScreen.java
@@ -4,24 +4,24 @@ import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 
 public class ConfigScreen {
     public static Screen build(Screen screen) {
         final ConfigBuilder builder = ConfigBuilder.create()
                 .setParentScreen(screen)
-                .setTitle(new LiteralText("Elytra Capes"))
+                .setTitle(Text.of("Elytra Capes"))
                 .setSavingRunnable(ConfigManager.writeConfig);
 
-        final ConfigCategory generalCategory = builder.getOrCreateCategory(new LiteralText("You should not see this"));
+        final ConfigCategory generalCategory = builder.getOrCreateCategory(Text.of("You should not see this"));
 
         final ConfigEntryBuilder entryBuilder = builder.entryBuilder();
 
-        generalCategory.addEntry(entryBuilder.startBooleanToggle(new LiteralText("Mod enabled"), ConfigManager.getConfig().modEnabled)
+        generalCategory.addEntry(entryBuilder.startBooleanToggle(Text.of("Mod enabled"), ConfigManager.getConfig().modEnabled)
                 .setSaveConsumer(newValue -> ConfigManager.getConfig().modEnabled = newValue)
                 .build());
 
-        generalCategory.addEntry(entryBuilder.startEnumSelector(new LiteralText("Cape style"), CapeStyleEnum.class, ConfigManager.getConfig().style)
+        generalCategory.addEntry(entryBuilder.startEnumSelector(Text.of("Cape style"), CapeStyleEnum.class, ConfigManager.getConfig().style)
                 .setSaveConsumer(newValue -> ConfigManager.getConfig().style = newValue)
                 .build());
 
